78b9a46d141ae7735f9537d06fc6380037fc2dbf
Shaik Azhar Ahmed <azzy049@gmail.com>
2021-08-09 21:35:02 +0530

    Completed solution #4  & #5

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask as Flask
 
+
 try:
     import sys
 except ImportError:
@@ -19,17 +20,55 @@ except ImportError:
     print('  json : library not found.  ')
     print('flask : Module not found')
 
-
+from helpers import utils
 
 data = []
 app = Flask(__name__)
 app.secret_key = 'Let it be a secrete'
 
 
+@app.route("/5", methods=['GET', 'POST'])
+def five_solution():
+    asset_metadata=[]
+   # response = utils.getAccessToken(2)
+ 
+    return render_template('5.html')
+
+@app.route("/ingest", methods=['GET', 'POST'])
+def ingest():
+    file_name = request.form.popitem()
+    asset_url = request.args.get('url')
+    asset_metadata=[]
+   # response = utils.getAccessToken(2)
+    print(file_name )
+    return jsonify("{'status' : 'ok'}")
+
+
+@app.route("/2", methods=['GET', 'POST'])
+def two_solution():
+    asset_metadata=[]
+    response = utils.getAccessToken(1)
+    asset_details = utils.get_videos(response)
+    
+    for asset in asset_details:
+        asset_metadata.append([asset['id'] , asset['name']])
+    print(asset_metadata)
+    return render_template('2.html',data=asset_metadata, act=response)
+
+@app.route("/1", methods=['GET', 'POST'])
+def one_solution():
+
+    return render_template('1.html')
+
+@app.route("/3", methods=['GET', 'POST'])
+def three_solution():
+
+    return render_template('3.html')
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
 
-    return render_template('2.html')
+    return render_template('3.html')
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
--- helpers/utils.py
+++ helpers/utils.py
@@ -1,7 +1,41 @@
 # global variables
+import requests
 error_class = 'alert alert-danger'
 success_class = 'alert alert-success'
 # checks if connected to the internal server : time out is 3 seconds
 # def isInternalNetwork():
 #     response =  os.system("ping -c 1 -W 3 " + "10.50.40.166")
 #     return False if (response>0) else True
+
+
+
+def getAccessToken(account):
+    url = "https://oauth.brightcove.com/v4/access_token?grant_type=client_credentials"
+
+    payload = {}
+    if(account == 1):
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic N2ZmNGEzZDAtZDRhMy00NjhlLWFjN2MtM2U3NDRjYjgzNDM2OjhRSHpkWjVkNGFVYU4yakxXLTlHVV83V3FtUENnakhyWU5oRmNaX3RiX3NUQS1vaXNyLVJHdlBndjUzOFAyY1hyMXc3cGhXQV9VUmhTUmxSa2c1Mll3',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, x-requested-with'
+        }
+    else:
+        headers = {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization': 'Basic YTkxMmQ5NmMtYjA5MC00YzAzLWIzOTctNjQ4Yjk5ZmJjNDVjOms2MEhneUwyYUhjM1Y1MGdlRFRqNEtvZThvakROVmFRXzV6bHhsMXljREI2MXlpeGJzbTNVU281RlM5SlplbENZX3g3SWV6SGRQVVV5RjdRQkU0UU1n',
+            'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE',
+            'Access-Control-Allow-Headers': 'Content-Type, x-requested-with'
+        }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    return response.json()['access_token']
+
+
+def get_videos(access_token):
+    url = "https://cms.api.brightcove.com/v1/accounts/5121028909001/videos"
+    payload = {}
+    headers = {
+        'Authorization': 'Bearer ' + access_token
+    }
+    response = requests.request("GET", url, headers=headers, data=payload)
+    return response.json()
